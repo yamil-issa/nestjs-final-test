132567b2fc6788b0cc9ffdf3e702eff9c500b291
Yamil <issayamilrt@gmail.com>
2024-05-21 16:26:11 +0200

    fix: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { AppRoutingModule } from './app.routing-module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
         AppRoutingModule,
         ConfigurationModule,
         DatabaseModule,
-        MongooseModule.forRoot('mongodb://localhost:24000/nestjs-final-test-db'),       
+        DatabaseModule,       
     ],
 })
 export class AppModule {}
--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigurationModule } from '../configuration/configuration.module';
+import { ConfigurationService } from '../configuration/configuration.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigurationModule],
+      useFactory: async (configService: ConfigurationService) => ({
+        uri: `mongodb://localhost:${configService.databaseConfig.DATABASE_PORT}/${configService.databaseConfig.DATABASE_NAME}`,
+      }),
+      inject: [ConfigurationService],
+    }),
+  ],
+  exports: [MongooseModule],
+})
 export class DatabaseModule {}
